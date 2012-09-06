a83999ad72983e57038b3535f515967cae79dd8c
Iván López <lopez.ivan@gmail.com>
2012-09-06 23:43:33 +0200

    Check the author and the joke before saving it

--- grails-app/services/com/chistacoOrNot/TwitterService.groovy
+++ grails-app/services/com/chistacoOrNot/TwitterService.groovy
@@ -10,40 +10,42 @@ class TwitterService {
 		}
 	}
 	
+	/**
+	 * Find tweets in twitter with the hashtag #chistaco
+	 *
+	 * @param page The page in twitter to search for jokes
+	 */
 	private void getTweetsFromPage(int page) {
 		def slurper = new JsonSlurper()
 
-		def url = "http://search.twitter.com/search.json?q=%23chistaco&rpp=100&page=${page}"
-		def content = new URL(url).getText("UTF-8")
-		def response = slurper.parseText(content)
+		try {
+			def url = "http://search.twitter.com/search.json?q=%23chistaco&rpp=100&page=${page}"
+			def content = new URL(url).getText("UTF-8")
+			def response = slurper.parseText(content)
 
-		def c = 0
-		response.results.each { tweet ->
-			
-			// Ignore the tweet if contains a RT or a '@'
-			if (!tweet.text.contains("RT") && !tweet.text.contains("@")) {
+			response.results.each { tweet ->
 				
-				println "-"*60
-				println tweet.text
-				println tweet.id
-				println "@${tweet.from_user} (${tweet.from_user_name})"
-				println tweet.profile_image_url
-				println tweet.from_user_id
-				println "-"*60
+				// Ignore the tweet if contains a RT or a '@'
+				if (!tweet.text.contains("RT") && !tweet.text.contains("@")) {
+					
+					// Check if the author already exists
+					def author = Author.findByTwitterId(tweet.from_user_id)
+					if (!author) {
+						author = new Author(name:tweet.from_user_name, twitter:tweet.from_user, twitterId:tweet.from_user_id, avatar:tweet.profile_image_url)
+					}
+					
+					// Check if the joke already exists
+					def joke = Joke.findByExternalJokeId(tweet.id)
+					if (!joke) {
+						joke = new Joke(text:tweet.text, externalJokeId:tweet.id)
+						author.addToJokes(joke)
+					}
 
-				def author = new Author(name:tweet.from_user_name, twitter:tweet.from_user, 
-					twitterId:tweet.from_user_id, avatar:tweet.profile_image_url)
-				
-				def joke = new Joke(description:tweet.text, externalJokeId:tweet.id)
-				
-				author.addToJokes(joke)
-				author.save()
-				
-				c++
+					author.save()
+				}
 			}
+		} catch (Exception e) {
+			log.error "There was an error with twitter"
 		}
-		
-		println "Total = ${c}"
-
 	}
 }
